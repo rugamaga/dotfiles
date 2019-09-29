2fa54f16302ed0e9a6bb70aa3c5a2cbeb4880d9a
rugamaga <rugamaga@gmail.com>
2019-09-29 14:16:30 +0900

    brewパッケージ管理も追加

---
+++ brew_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# package management for appstore
+brew install mas
+
+# basic tools
+brew install curl
+brew install git
+brew install nkf
+brew install zsh
+brew install neovim
+brew install ripgrep
+
+# for development audio application with rust...
+brew install cmake
+brew install pkg-config
+brew install portaudio
